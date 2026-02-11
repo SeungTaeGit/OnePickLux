2db8fa48ac761aa2c6ea53ba4b896d39d400c4bb
SeungTaeGit <dltmdxo1589@daum.net>
2026-02-11 12:51:02 +0900

    First Commit

---
+++ src/main/java/com/onepicklux/OnepickLuxApplication.java
@@ -0,0 +1,13 @@
+package com.onepicklux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnepickLuxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnepickLuxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onepicklux/OnepickLuxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onepicklux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnepickLuxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
